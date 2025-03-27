44fc536501f9425cf0493d1a02aa7f6b599a114f
arpitashanbhag01 <148280369+arpitashanbhag01@users.noreply.github.com>
2025-03-27 14:08:00 -0700

    Update options.py

--- options.py
+++ options.py
@@ -18,5 +18,16 @@ adding = input("")
 while adding != "No":
     student = input("Which student would you like to calculate the overall grade for?: ").capitalize()
     for student, value in classdict.items():
-        print(sum(value)/len(value))
+        overall = (sum(value)/len(value))
+        if overall >= 89.5:
+            print(overall, "A")
+        elif (overall <= 89.5 and overall >= 79.5):
+            print(overall, "B")
+        elif (overall <= 79.5 and overall >= 69.5):
+            print(overall, "C")
+        elif (overall <= 69.5 and overall >= 59.5):
+            print(overall, "D")
+        else:
+            print(overall, "F")
+    adding = input("Would you like to calculate another student's grade (yes/no): ").capitalize()
 print(classdict)
