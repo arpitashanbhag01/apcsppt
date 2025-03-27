3abd54da023afc5a5ab16449a1cd38a1ced0ad50
arpitashanbhag01 <148280369+arpitashanbhag01@users.noreply.github.com>
2025-03-27 14:30:39 -0700

    Update options.py

--- options.py
+++ options.py
@@ -1,22 +1,40 @@
 classdict = {}
 adding = input("")
 while adding != "No":
-    studentname = input("Please enter the name of the student: ").capitalize()
-    adding = input("Would you like to add another student to your class (yes/no): ").capitalize()
+    #deal with repeating names
+    studentname = input("Please enter the name of the student: ").strip().capitalize()
+    while True:
+        adding = input("Would you like to add another student to your class (yes/no): ").strip().capitalize()
+        if adding in ("Yes", "No"):
+            break
+        else:
+            print("Invalid input. Please enter 'Yes' or 'No'.")
     classdict[studentname] = []
 print(classdict)
 
 adding = input("")
 while adding != "No":
-    student = input("Which student would you like to add a grade for?: ").capitalize()
+    while True:
+        student = input("Which student would you like to add a grade for?: ").strip().capitalize()
+        if student in classdict.keys():
+            break
+        else:
+            print("This student is not part of your list.")
+            #would you like to add them to your list
+    #check if their input is an integer .isdigit()
     grade = int(input("Enter their grade: "))
     classdict[student].append(grade)
-    adding = input("Would you like to add another grade (yes/no): ").capitalize()
+    adding = input("Would you like to add another grade (yes/no): ").strip().capitalize()
 print(classdict)
 
 adding = input("")
 while adding != "No":
-    student = input("Which student would you like to calculate the overall grade for?: ").capitalize()
+    while True:
+        student = input("Which student would you like to calculate the overall grade for?: ").strip().capitalize()
+        if student in classdict.keys():
+            break
+        else:
+            print("This student is not part of your list.")
     for student, value in classdict.items():
         overall = (sum(value)/len(value))
         if overall >= 89.5:
@@ -29,5 +47,5 @@ while adding != "No":
             print(overall, "D")
         else:
             print(overall, "F")
-    adding = input("Would you like to calculate another student's grade (yes/no): ").capitalize()
+    adding = input("Would you like to calculate another student's grade (yes/no): ").strip().capitalize()
 print(classdict)
