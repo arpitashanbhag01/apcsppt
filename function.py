c897efdda722ff7287bb7c936eb5d94817cb23ae
arpitashanbhag01 <148280369+arpitashanbhag01@users.noreply.github.com>
2025-04-04 12:38:41 -0700

    Update function.py

--- function.py
+++ function.py
@@ -5,27 +5,39 @@ def addstudent(studentname):
             classdict[studentname] = []
             break
         else:
-            print(studentname, "is already in the class list. Please list a different student.")
+            print(studentname + " is already in the class list.")
+            studentname = input("Please list a different student: ").strip().capitalize()
 
 def calculategrade(student):
-    for student, value in classdict.items():
-        overall = (sum(value)/len(value))
+    gradelist = classdict.get(student)
+    print(gradelist)
+    grade = 0
+    for i in gradelist:
+        grade += i
+        overall = (grade/len(gradelist))
         if overall >= 89.5:
-            print(overall, "A")
+            print(student, overall, "A")
         elif (overall <= 89.5 and overall >= 79.5):
-            print(overall, "B")
+            print(student, overall, "B")
         elif (overall <= 79.5 and overall >= 69.5):
-            print(overall, "C")
+            print(student, overall, "C")
         elif (overall <= 69.5 and overall >= 59.5):
-            print(overall, "D")
+            print(student, overall, "D")
         else:
-            print(overall, "F")
+            print(student, overall, "F")
 
-numstudent = int(input("How many students would you like to add to your class?: "))
+while True:
+  try:
+    numstudent = int(input("How many students would you like to add to your class?: "))
+    break
+  except ValueError:
+    print("Invalid input. Please enter an integer.")
+
 for i in range (numstudent):
     studentname = input("Please enter the name of the student: ").strip().capitalize()
     addstudent(studentname)
 print(classdict)
+
 adding = input("")
 while adding != "No":
     while True:
@@ -33,17 +45,21 @@ while adding != "No":
         if student in classdict.keys():
             break
         else:
-            print("This student is not part of your list.")
+            print("This student is not part of your class.")
             #would you like to add them to your list
     while True:
+      try:
         grade = int((input("Enter their test score: ")))
-        #fix isdigit
-        if grade.isdigit():
-            classdict[student].append(grade)
-            break
-        else:
-            print("Please enter a valid test score.")
+        break
+      except ValueError:
+        print("Please enter a valid test score.")
+    
+    classdict[student].append(grade)
     adding = input("Would you like to add another test score (yes/no): ").strip().capitalize()
+
+gradelist = classdict.get("Arpita")
+print(gradelist)  
+
 whostudent = input("Who do you want to calculate a grade for?: ")
 calculategrade(whostudent)
 print(classdict)
